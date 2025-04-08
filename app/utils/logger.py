8c16cb808ef9b14dcf42cf91e031b3f44bec75a4
muhammedkh45 <127145833+muhammedkh45@users.noreply.github.com>
2025-04-08 17:23:03 +0200

    deleted files

--- app/utils/logger.py
+++
@@ -1,6 +0,0 @@
-# Basic APILog functionality
-def log_request(endpoint: str, data: dict):
-    print(f"Logging request to {endpoint}: {data}")
-
-def log_response(status: int, response_data: dict):
-    print(f"Logging response with status {status}: {response_data}")
