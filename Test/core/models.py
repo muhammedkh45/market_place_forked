6c9cc0b204a598e382bdf26ee567f0f7f1e69111
youssefosama0988 <youssefosama789456@gmail.com>
2025-04-23 11:21:05 +0200

    adjust the top-right profile icon in navbar to make the user image appear

--- Test/core/models.py
+++ Test/core/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from datetime import date
 
 class ContactMessage(models.Model):
     name = models.CharField(max_length=100)
@@ -22,13 +23,14 @@ class UserProfile(models.Model):
     date_of_birth = models.DateField(blank=True, null=True)
     age = models.PositiveIntegerField(blank=True, null=True)
     balance = models.PositiveIntegerField(default=1000)
+
     def get_age(self):
         if self.date_of_birth:
-            from datetime import date
             today = date.today()
             age = today.year - self.date_of_birth.year - ((today.month, today.day) < (self.date_of_birth.month, self.date_of_birth.day))
             return age
         return None
+    
     @staticmethod
     def get_profile_by_user(user):
         try:
