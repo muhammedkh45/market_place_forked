966fa05d5ec31b84bcd6f6e2c86bc07f809a30b9
hana-shamel <hana.s.lotfy@gmail.com>
2025-04-25 18:40:44 +0300

    Refactor database path to use BASE_DIR for better portability

--- Test/Test/settings.py
+++ Test/Test/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'Test.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'C:/Users/hanas/OneDrive/Desktop/depo/Marketplace/Test/db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
