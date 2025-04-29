129fa77a74b2706074436e1e20ccf1a049bf4806
Sama-Ahmed-Mohammed <167349546+Sama-Ahmed-Mohammed@users.noreply.github.com>
2025-04-30 01:09:51 +0300

    Update views.py

--- Test/inventory/views.py
+++ Test/inventory/views.py
@@ -1,14 +1,13 @@
-from django.shortcuts import render, redirect
-
-from .models import Account  
+from django.shortcuts import render
+from core.models import UserProfile
 
 def account_page(request):
-    account = None  # default
+    user_profile = None
 
     if request.user.is_authenticated:
         try:
-            account = Account.objects.get(user=request.user)
-        except Account.DoesNotExist:
-            account = None
+            user_profile = UserProfile.objects.get(user=request.user)
+        except UserProfile.DoesNotExist:
+            user_profile = None
 
-    return render(request, 'inventory.html', {'account': account})
+    return render(request, 'inventory.html', {'profile': user_profile})
