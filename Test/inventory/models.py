2f5e8d62002e432ee0a2edee92aaac7df8589265
Sama-Ahmed-Mohammed <167349546+Sama-Ahmed-Mohammed@users.noreply.github.com>
2025-04-30 00:31:50 +0300

    Update models.py

--- Test/inventory/models.py
+++ Test/inventory/models.py
@@ -1,3 +1,25 @@
+
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    cash_balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    
+    class Meta:
+        app_label = 'inventory'
+
+class Item(models.Model):
+    STATUS_CHOICES = (
+        ('purchased', 'Purchased'),
+        ('sold', 'Sold'),
+        ('available', 'Available'),
+    )
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='available')
+    created_at = models.DateTimeField(auto_now_add=True)
