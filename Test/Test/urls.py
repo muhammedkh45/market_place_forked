2c7e91e15eddae721b53243b5ed4a2fe5d07009b
MoustafaHashem <moustafahashem7770@gmail.com>
2025-04-29 22:55:13 +0300

    Add inventory app with basic structure and navbar integration

--- Test/Test/urls.py
+++ Test/Test/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('deposit/',include('deposit.urls')),
     path('home/', include('carts.urls')),
     path('dashboard/', include('dashboard.urls')),
+    path('inventory/', include('inventory.urls')),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
