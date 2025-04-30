2a3bd7fcd4c8f5a23f5c95d934e9e948d4799340
MoustafaHashem <moustafahashem7770@gmail.com>
2025-04-30 16:38:52 +0300

    Refactor code structure for improved readability and maintainability

--- Test/inventory/urls.py
+++ Test/inventory/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.show,name='Inventory'),
+    path('',views.account_page,name='Inventory'),
     
 ]
--- Test/inventory/views.py
+++ Test/inventory/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from core.models import UserProfile
+from items.models import Items
 
 def account_page(request):
     user_profile = None
+    available_items = None
 
     if request.user.is_authenticated:
         try:
             user_profile = UserProfile.objects.get(user=request.user)
+            available_items = Items.objects.filter(owned_by=user_profile)
+            
         except UserProfile.DoesNotExist:
             user_profile = None
+            available_items = None
 
-    return render(request, 'inventory.html', {'profile': user_profile})
+    return render(request, 'inventory/inventory.html', {'available_items': available_items})
