dc3ee7f5f035a3d177e661011563b189ce27851a
MoustafaHashem <moustafahashem7770@gmail.com>
2025-05-04 22:08:13 +0300

    .

--- SALES_square/Test/settings.py
+++ SALES_square/Test/settings.py
@@ -16,7 +16,7 @@ import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-CSRF_TRUSTED_ORIGINS= ['marketplace-production-ba97.up.railway.app/']
+CSRF_TRUSTED_ORIGINS= ["https://marketplace-production-ba97.up.railway.app/"]
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
